c5739aa511063d4035ba958377132ed373161c56
SamuelFoo <fooenzesamuel@gmail.com>
2024-09-15 10:32:50 +0800

    Add mediapipe to backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,28 @@
 import cv2
+import mediapipe as mp
+import numpy as np
 from flask import Flask, Response
 
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
 app = Flask(__name__)
+
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  # First
+    b = np.array(b)  # Mid
+    c = np.array(c)  # End
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(
+        a[1] - b[1], a[0] - b[0]
+    )
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
 
 
 # Route to stream video feed
@@ -9,21 +30,144 @@ app = Flask(__name__)
 def video_feed():
     def generate_frames():
         cap = cv2.VideoCapture(0)  # Capture video from the webcam
-        while True:
-            success, frame = cap.read()
-            if not success:
-                break
-            else:
+
+        # Curl counter variables
+        counter = 0
+        stage = None
+
+        ## Setup mediapipe instance
+        with mp_pose.Pose(
+            min_detection_confidence=0.5, min_tracking_confidence=0.5
+        ) as pose:
+            while cap.isOpened():
+                ret, frame = cap.read()
+
+                # Recolor image to RGB
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False
+
+                # Make detection
+                results = pose.process(image)
+
+                # Recolor back to BGR
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+                # Extract landmarks
+                try:
+                    landmarks = results.pose_landmarks.landmark
+
+                    # Get coordinates
+                    shoulder = [
+                        landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                        landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y,
+                    ]
+                    elbow = [
+                        landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                        landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y,
+                    ]
+                    wrist = [
+                        landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                        landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y,
+                    ]
+
+                    # Calculate angle
+                    angle = calculate_angle(shoulder, elbow, wrist)
+
+                    # Visualize angle
+                    cv2.putText(
+                        image,
+                        str(angle),
+                        tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        (255, 255, 255),
+                        2,
+                        cv2.LINE_AA,
+                    )
+
+                    # Curl counter logic
+                    if angle > 160:
+                        stage = "down"
+                    if angle < 30 and stage == "down":
+                        stage = "up"
+                        counter += 1
+                        print(counter)
+
+                except:
+                    pass
+
+                # Render curl counter
+                # Setup status box
+                cv2.rectangle(image, (0, 0), (225, 73), (245, 117, 16), -1)
+
+                # Rep data
+                cv2.putText(
+                    image,
+                    "REPS",
+                    (15, 12),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 0, 0),
+                    1,
+                    cv2.LINE_AA,
+                )
+                cv2.putText(
+                    image,
+                    str(counter),
+                    (10, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    2,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA,
+                )
+
+                # Stage data
+                cv2.putText(
+                    image,
+                    "STAGE",
+                    (65, 12),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 0, 0),
+                    1,
+                    cv2.LINE_AA,
+                )
+                cv2.putText(
+                    image,
+                    stage,
+                    (60, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    2,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA,
+                )
+
+                # Render detections
+                mp_drawing.draw_landmarks(
+                    image,
+                    results.pose_landmarks,
+                    mp_pose.POSE_CONNECTIONS,
+                    mp_drawing.DrawingSpec(
+                        color=(245, 117, 66), thickness=2, circle_radius=2
+                    ),
+                    mp_drawing.DrawingSpec(
+                        color=(245, 66, 230), thickness=2, circle_radius=2
+                    ),
+                )
+
                 # Encode frame as JPEG
-                ret, buffer = cv2.imencode(".jpg", frame)
-                frame = buffer.tobytes()
+                ret, buffer = cv2.imencode(".jpg", image)
+                image = buffer.tobytes()
 
                 # Stream frame
                 yield (
-                    b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+                    b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + image + b"\r\n"
                 )
 
-        cap.release()
+            cap.release()
 
     return Response(
         generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
